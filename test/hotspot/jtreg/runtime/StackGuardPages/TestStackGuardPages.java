2e543657e1093da923cbd6246fa942fe051adb75
Kurt Miller <bsdkurt@gmail.com>
2025-10-08 15:28:19 -0400

    Disable test_java_overflow_initial on OpenBSD OpenBSD's primordial thread stack permissions are immutable and therefore cannot set stack guard pages there. Disable testing for just the primordial thread on OpenBSD.

--- test/hotspot/jtreg/runtime/StackGuardPages/TestStackGuardPages.java
+++ test/hotspot/jtreg/runtime/StackGuardPages/TestStackGuardPages.java
@@ -30,6 +30,7 @@
  * @compile DoOverflow.java
  * @run main/native TestStackGuardPages
  */
+import jdk.test.lib.Platform;
 import jdk.test.lib.Utils;
 import jdk.test.lib.process.ProcessTools;
 import jdk.test.lib.process.OutputAnalyzer;
@@ -43,11 +44,13 @@ public class TestStackGuardPages {
         OutputAnalyzer output = ProcessTools.executeProcess(pb);
         output.shouldHaveExitValue(0);
 
-        pb = ProcessTools.createNativeTestProcessBuilder("invoke",
-                                                         "test_java_overflow_initial");
-        pb.environment().put("CLASSPATH", Utils.TEST_CLASS_PATH);
-        output = ProcessTools.executeProcess(pb);
-        output.shouldHaveExitValue(0);
+        if (!Platform.getOsName().equals("OpenBSD")) {
+            pb = ProcessTools.createNativeTestProcessBuilder("invoke",
+                                                             "test_java_overflow_initial");
+            pb.environment().put("CLASSPATH", Utils.TEST_CLASS_PATH);
+            output = ProcessTools.executeProcess(pb);
+            output.shouldHaveExitValue(0);
+        }
     }
 }
 
